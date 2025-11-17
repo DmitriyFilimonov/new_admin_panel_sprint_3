2bcb1eab02b62066d418d52ca9c06c093810bc0e
Dmitriy Filimonov <johndoe@example.com>
2025-11-17 11:03:58 +0300

    cut rudiments

--- etl/process/es_client.py
+++ etl/process/es_client.py
@@ -87,12 +87,6 @@ ES_URL = f"http://{ES_HOST}:{ES_PORT}"
 
 
 @backoff(border_sleep_time=60, factor=1.5, start_sleep_time=10)
-def test_es():
-    r = requests.get(f"{ES_URL}")
-    print(r.json(), flush=True)
-
-
-@backoff(border_sleep_time=60, factor=1.5, start_sleep_time=10)
 def create_scheme():
     headers = {"Content-Type": "application/json"}
     requests.put(f"{ES_URL}/movies", json=schema, headers=headers)
